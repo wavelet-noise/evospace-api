d227cb6dc68a069f88a1b189f9158006c426bd25
wavelet-noise <ishellstrike@gmail.com>
2022-12-12 04:39:45 +0200

    postprocess script

--- filter_ue4_macros.py
+++ filter_ue4_macros.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import os
 import re
 import sys
 
@@ -5,14 +8,15 @@ import sys
 def paren_matcher (n):
     return r"[^()]*?(?:\("*n+r"[^()]*?"+r"\)[^()]*?)*?"*n
 
-for filename in os.listdir("."):
-    f = os.path.join(directory, filename)
-    # checking if it is a file
-    if os.path.isfile(f):
-        # Slurp file into a single string
-        with open(filename, 'r') as file:
-            content = file.read()
-            regex = '^(\s*)((?:UFUNCTION|UCLASS|UPROPERTY|UENUM)\s*\('+paren_matcher(25)+'\))'
-            content = re.sub(regex, r'\1/* UE4 Macro: \2 */', content, flags=re.MULTILINE)
-        with open(filename, 'w') as file:
-            file.write(content)
+for root, dirs, files in os.walk("./"):
+    for filename in files:
+        namepath = os.path.join(root, filename)
+        if namepath.find(".h") != -1:
+            print(namepath)
+            # Slurp file into a single string
+            with open(namepath, 'r') as file:
+                content = file.read()
+                regex = '^(\s*)((?:UFUNCTION|UCLASS|UPROPERTY|UENUM)\s*\('+paren_matcher(25)+'\))'
+                content = re.sub(regex, r'\1/* UE4 Macro: \2 */', content, flags=re.MULTILINE)
+            with open(namepath, 'w') as file:
+                file.write(content)
