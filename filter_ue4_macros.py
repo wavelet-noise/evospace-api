ff2552c08bff9ddc8b2239d3a8e3e3b2683738e9
wavelet-noise <ishellstrike@gmail.com>
2022-12-12 05:23:01 +0200

    postprocess attempt

--- filter_ue4_macros.py
+++ filter_ue4_macros.py
@@ -16,7 +16,7 @@ for root, dirs, files in os.walk("./"):
             # Slurp file into a single string
             with open(namepath, 'r') as file:
                 content = file.read()
-                regex = '^(\s*)((?:UFUNCTION|UCLASS|UPROPERTY|UENUM)\s*\('+paren_matcher(25)+'\))'
+                regex = '^(\s*)((?:UFUNCTION|UCLASS|UPROPERTY|UENUM|GENERATED_BODY)\s*\('+paren_matcher(25)+'\))'
                 content = re.sub(regex, r'\1/* UE4 Macro: \2 */\n\n', content, flags=re.MULTILINE)
             with open(namepath, 'w') as file:
                 file.write(content)
