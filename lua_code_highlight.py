4389d67b89fea3564657c8480cd15a569ee28e05
wavelet-noise <ishellstrike@gmail.com>
2023-02-08 21:34:28 +0200

    code formatter

--- lua_code_highlight.py
+++ lua_code_highlight.py
@@ -2,32 +2,126 @@
 
 import os
 import re
-import string
-import sys
 
+LUATOKENS = [
+    (r'\b(and|break|do|else|elseif|end|false|for|function|if|in|local|nil|not|or|repeat|return|then|true|until|while)\b', 'keyword'),
+    (r'[+-]?\d+\.\d+', 'number'),
+    (r'[+-]?\d+', 'number'),
+    (r'".*?"', 'string'),
+    (r"'.*?'", 'string'),
+    (r'[\+\-\*\/\^%%#=<>~]', 'operator'),
+    (r'[\[\]\{\}\(\)\.,;:]', 'punctuation'),
+    (r'\b(and|or)\b', 'boolean'),
+    (r'\b(nil)\b', 'null'),
+    (r'[a-zA-Z_][a-zA-Z0-9_]*', 'identifier'),
+    (r'\n', 'newline'),
+    (r'--[^\n]*', 'comment')
+]
 
-def paren_matcher (n):
-    return r'[^()]*?(?:\("*n+r"[^()]*?"+r"\)[^()]*?)*?'*n
+def tokenize_lua(code):
+    tokens = []
+    i = 0
+    while i < len(code):
+        for pattern, token_type in LUATOKENS:
+            match = re.match(pattern, code[i:])
+            if match:
+                value = match.group(0)
+                if token_type == 'string':
+                    value = value[1:-1]
+                tokens.append((token_type, value))
+                i += len(value)
+                break
+        else:
+            i += 1
+    return tokens
 
-def replace_with_span(line, word, color):
-    return re.sub(r'(?<!<.*)text(?!.*>)', 
-    lambda x: x.group().replace(word, r'<span class=“line” style="color: ' + color + '">' + word + r'</span>'), line)
+JSONTOKENS = [
+    (r'\b(true|false|null)\b', 'boolean'),
+    (r'-?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?', 'number'),
+    (r'"(?:\\.|[^"\\])*"', 'string'),
+    (r'[\[\]\{\}\:,]', 'punctuation'),
+    (r'\n', 'newline')
+]
 
-def replace_quotes(line, color):
-    quotes = re.findall(r'"(.*?)"', line)
-    for quote in quotes:
-        line = line.replace('"' + quote + '"', r'<span class=“line” style="color: ' + color + '">"' + quote  + r'"</span>')
-    
-    return line
+def tokenize_json(code):
+    tokens = []
+    i = 0
+    while i < len(code):
+        for pattern, token_type in JSONTOKENS:
+            match = re.match(pattern, code[i:])
+            if match:
+                value = match.group(0)
+                if token_type == 'string':
+                    value = value[1:-1].replace('\\"', '"')
+                tokens.append((token_type, value))
+                i += len(value)
+                break
+        else:
+            i += 1
+    return tokens
 
-def replace_comment(line, color):
-    quotes = re.findall(r'^--.*$', line)
-    for quote in quotes:
-        line = line.replace(quote, r'<span class=“line” style="color: ' + color + '">"' + quote  + r'"</span>')
-    
-    return line
+TOKEN_COLORS = {
+    'text': '#D4D4D4',
+    'newline': '#D4D4D4',
+    'keyword': '#569CD6',
+    'string': '#CE9178',
+    'number': '#B5CEA8',
+    'operator': '#D4D4D4',
+    'punctuation': '#D4D4D4',
+    'boolean': '#B5CEA8',
+    'null': '#CE9178',
+    'identifier': '#DCDCAA',
+    'comment': '#DCFFAA',
+}
+TOKEN_DEFAULT_COLOR = '#D4D4D4'
 
-red_color = '#E17873'
+# Regular expression to find the code blocks
+code_block_regex = re.compile(r'```(lua|json)([\s\S]+?)```|([\s\S]+?)')
+
+def extract_code_blocks(text):
+    code_blocks = []
+    pos = 0
+    while pos < len(text):
+        match = code_block_regex.match(text, pos)
+        if match.group(1):
+            code_type = match.group(1)
+            code = match.group(2)
+            code_blocks.append((code_type, code))
+        else:
+            code_blocks.append(('text', match.group(3)))
+        pos = match.end()
+    return code_blocks
+
+def decorate_tokens(tokens):
+    html = ""
+    for token in tokens:
+        if token[0] == 'newline':
+            html += '<br>\n'
+        elif token[0] == 'whitespace':
+            html += ' '
+        else:
+            color = TOKEN_COLORS[token[0]]
+            html += '<span style="color: ' + color + '">' + token[1] + '</span>'
+            
+    return html
+
+def decorate_codeblocks(code_blocks):
+    md = ""
+    for code_block in code_blocks:
+        if code_block[0] == 'lua':
+            md += '<div class="fragment">\n'
+            tokens = tokenize_lua(code_block[1])
+            md += decorate_tokens(tokens)
+            md += '</div">\n'
+        elif code_block[0] == 'json':
+            md += '<div class="fragment">\n'
+            tokens = tokenize_json(code_block[1])
+            md += decorate_tokens(tokens)
+            md += '</div">\n'
+        else:
+            md += code_block[1]
+    return md
+
 
 for root, dirs, files in os.walk('./'):
     for filename in files:
@@ -38,35 +132,10 @@ for root, dirs, files in os.walk('./'):
             new_content = ''
             now_parsing = 'none'
             with open(namepath, 'r') as file:
-                content = file.read()
-                lines = content.splitlines()
-                for line in lines:
-                    if line.find('```lua') != -1:
-                        now_parsing = 'lua'
-                        new_content += '\n<div class="fragment">'
-                        line = line.replace('```lua', '')
-                    elif line.find('```json') != -1:
-                        now_parsing = 'json'
-                        new_content += '\n<div class="fragment">'
-                        line = line.replace('```json', '')
-                    elif line.find('```') != -1:
-                        now_parsing = 'none'
-                        new_content += '\n</div>'
-                        line = line.replace('```', '')
-
-                    if now_parsing == 'lua':
-                        line = replace_quotes(line, '#C89682')
-                        line = replace_comment(line, '#C8FF82')
-
-                        #line = replace_with_span(line, 'function', red_color)
-                        #line = replace_with_span(line, 'print', red_color)
-                        #line = replace_with_span(line, 'end', red_color)
-                        #line = replace_with_span(line, 'then', red_color)
-                        #line = replace_with_span(line, 'do', red_color)
-                        #line = replace_with_span(line, 'local', red_color)
-                        #line = replace_with_span(line, '=', red_color)
-
-                    new_content += line
-                    new_content += '\n<div>'
+                code = file.read()
+                
+                code_blocks = extract_code_blocks(code)
+                md = decorate_codeblocks(code_blocks)
+                
             with open(namepath, 'w') as file:
-                file.write(new_content)
+                file.write(md)
