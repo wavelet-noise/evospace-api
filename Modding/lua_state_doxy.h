3e60f0df93a68291e826c7c85a5b7210e47d3355
wavelet-noise <ishellstrike@gmail.com>
2023-02-24 16:13:08 +0200

    test

--- Modding/lua_state_doxy.h
+++ Modding/lua_state_doxy.h
@@ -3,8 +3,20 @@
 /*! \file lua_state_doxy.h
     \brief Vec3 struct
 */
+
+/*! \class Vec3
+    \brief 3d float vector
+*/
  
 /*! \fn Vec3 new(int32 x, int32 y, int32 z)
     \brief Static function. \a fd.
     \param fd The descriptor to close.
+*/
+
+/*! \class Vec3i
+    \brief 3d int vector
+*/
+
+/*! \class Vec2i
+    \brief 2d int vector
 */
