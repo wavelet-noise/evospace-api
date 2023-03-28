8eec611e608feaec2fc391155e2e18032ed631d1
wavelet-noise <ishellstrike@gmail.com>
2023-03-29 02:04:19 +0300

    fvec

--- filter_ue4_macros.py
+++ filter_ue4_macros.py
@@ -52,11 +52,11 @@ for root, dirs, files in os.walk("./"):
             # Slurp file into a single string
             with open(namepath, 'r') as file:
                 content = file.read()
+                content = content.replace('FVector', 'Vec3')
                 content = replace_camel_case_with_snake_case(content)
                 regex = '^(\s*)((?:UFUNCTION|UCLASS|UPROPERTY|UENUM|GENERATED_BODY)\s*\('+paren_matcher(25)+'\))'
                 content = re.sub(regex, r'\1', content, flags=re.MULTILINE)
                 content = re.sub(r'\bU[A-Z]\w+', lambda x: x.group().replace("U", ""), content)
                 content = re.sub(r'\bF[A-Z]\w+', lambda x: x.group().replace("F", ""), content)
-                content = content.replace('FVector', 'Vec3')
             with open(namepath, 'w') as file:
                 file.write(content)
