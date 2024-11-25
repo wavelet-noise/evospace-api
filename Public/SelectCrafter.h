7a7c92e169b682b31591149b967cfdfd451cc0c6
wavelet-noise <ishellstrike@gmail.com>
2024-11-25 23:51:17 +0200

    ref

--- Public/SelectCrafter.h
+++ Public/SelectCrafter.h
@@ -18,6 +18,12 @@ UCLASS(BlueprintType)
  */
 class EVOSPACE_API USelectCrafter : public UAbstractCrafter {
   using Self = USelectCrafter;
+  EVO_CODEGEN_INSTANCE(SelectCrafter)
+  virtual void lua_reg(lua_State *L) const override {
+      luabridge::getGlobalNamespace(L)
+        .deriveClass<Self, UAbstractCrafter>("SelectCrafter")
+        .endClass();
+    }
   GENERATED_BODY()
 
   public:
@@ -62,11 +68,4 @@ class EVOSPACE_API USelectCrafter : public UAbstractCrafter {
   //TODO: replace with output container
   UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
   UInventoryContainer *mAutoCrafterCoreOutput;
-
-  EVO_CODEGEN_INSTANCE(SelectCrafter)
-  virtual void lua_reg(lua_State *L) const override {
-    luabridge::getGlobalNamespace(L)
-      .deriveClass<Self, UAbstractCrafter>("SelectCrafter")
-      .endClass();
-  }
 };
